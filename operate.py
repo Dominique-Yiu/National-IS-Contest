b14ec188834b7a6a5b03ea04698fdd06adbf1b67
Dominique-Yiu <1611440536@qq.com>
2022-04-23 23:12:35 +0800

    2022/4/23 23:12

--- operate.py
+++ operate.py
@@ -21,13 +21,14 @@ def eliminate(data=None):
 class operate(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(operate, self).__init__()
+        self.processing_thread = None
         self.setupUi(self)
         self.collector = collect_data(port='COM5', m_time=1)
 
     def processing_data(self):
         name = self.lineEdit_2.text()
         gross_data = []
-        for i in range(8):
+        for _ in range(8):
             filtered_data = self.collector.start(load_path=None, save_path=None, m_time=10)
             # remove the unstable data (the first 0.5 seconds)
             filtered_data = filtered_data[int(2302 * 0.5):]
@@ -47,8 +48,8 @@ class operate(QtWidgets.QMainWindow, Ui_MainWindow):
         add_modify(add_data=gross_data, add_name=name)
 
     def start_measure(self):
-        threading.Thread(target=self.processing_data)
-        self.recv_thread.start()
+        self.processing_thread = threading.Thread(target=self.processing_data)
+        self.processing_thread.start()
         #     self.textBrowser_2.setText(f"已收集第{i}次数据")
         # QMessageBox.information(self, '提示信息', '按钮1被按下。')
 
