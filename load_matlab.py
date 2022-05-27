b56f0b776a081817aa910bd90c7eb8dee3dfd8de
Dominique-Yiu <1611440536@qq.com>
2022-05-27 21:47:12 +0800

    5/27 21:47

--- load_matlab.py
+++ load_matlab.py
@@ -3,20 +3,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 eng = matlab.engine.start_matlab()
-ev_data = np.loadtxt('envelope_data_3.csv')
+ev_data = np.loadtxt('./output_data/smooth_data_LXY_20.csv')
+# plt.plot(ev_data)
+# plt.show()
 ev_data = matlab.double(initializer=list(ev_data), size=(1, len(ev_data)), is_complex=False)
-# new_start, data_time, data_seg = eng.patterMatch(ev_data, 3, nargout=3)
-# y = np.array(new_start[0])
-# print(y)
-
-# print(np.squeeze(np.array(new_start)))
-
-
-
-C = eng.smoothdata(ev_data, 'gaussian', 400, nargout=1)
-plt.plot(C[0])
-plt.show()
-
+smoothData = eng.smoothdata(ev_data, 'gaussian', 400, nargout=1)
+# plt.plot(smoothData[0])
+# plt.show()
+new_start, data_time, data_seg, time = eng.patterMatch(smoothData, 3, nargout=4)
+print(new_start)
+def show(raw_time, raw_data,  start_time, start_seg):
+    plt.figure(figsize=(16, 9))
+    plt.plot(raw_time, raw_data)
+    plt.draw()
+    cnt = start_time.size[0]
+    for idx in range(cnt):
+        plt.plot(start_time[idx], start_seg[idx])
+        plt.draw()
+    plt.show()
+print(data_time.size)
+show(raw_time=time[0], raw_data=smoothData[0], start_time=data_time, start_seg=data_seg)
 
 
 
--- patterMatch.m
+++ patterMatch.m
@@ -1,21 +1,52 @@
-function [new_start, data_time, data_seg] = patterMatch(ev_value, rhythm_number)
+function [new_start, data_time, data_seg, time] = patterMatch(ev_value, rhythm_number)
+
+    % Fit computation did not converge:
+    % Fitting stopped because the number of iterations or function evaluations exceeded the specified maximum.
+    
+    % Fit found when optimization terminated:
+    
+    % General model Gauss4:
+    %      f(x) = 
+    %               a1*exp(-((x-b1)/c1)^2) + a2*exp(-((x-b2)/c2)^2) + 
+    %               a3*exp(-((x-b3)/c3)^2) + a4*exp(-((x-b4)/c4)^2)
+    % Coefficients (with 95% confidence bounds):
+    %        a1 =       16.65  (9.804, 23.49)
+    %        b1 =       172.1  (165.8, 178.4)
+    %        c1 =       136.4  (121.6, 151.2)
+    %        a2 =       11.89  (5.747, 18.04)
+    %        b2 =       287.9  (189.8, 386)
+    %        c2 =       248.7  (185.7, 311.6)
+    %        a3 =       275.5  (-1356, 1907)
+    %        b3 =        -148  (-1337, 1040)
+    %        c3 =        1175  (-633.4, 2984)
+    %        a4 =         272  (156.8, 387.3)
+    %        b4 =        2233  (1865, 2600)
+    %        c4 =        3177  (-2.571e+04, 3.207e+04)
+    
+    % Goodness of fit:
+    %   SSE: 276.9
+    %   R-square: 0.9999
+    %   Adjusted R-square: 0.9999
+    %   RMSE: 0.4313    
+    
+    
 
     time = 1/2302:1/2302:length(ev_value)/2302;
 
-    a1 =       15.93;
-    b1 =       5.726;
-    c1 =      0.0453;
-    a2 =       28.79;
-    b2 =       5.765;
-    c2 =      0.1749;
-    a3 =       56.32;
-    b3 =       5.804;
-    c3 =      0.3934;
-    a4 =         743;
-    b4 =      0.7234;
-    c4 =       6.066;
+    a1 =       16.65;
+    b1 =       172.1;
+    c1 =        136.4;
+    a2 =       11.89;
+    b2 =       287.9;
+    c2 =       248.7;
+    a3 =       275.5;
+    b3 =          -148;
+    c3 =        1175;
+    a4 =       272;
+    b4 =        2233;
+    c4 =        3177;
 
-    load 'time_1.mat'
+    x = 0:1500;
     L = a1*exp(-((x-b1)/c1).^2) + a2*exp(-((x-b2)/c2).^2) + a3*exp(-((x-b3)/c3).^2) + a4*exp(-((x-b4)/c4).^2);
 
     heng = []; zong = [];c = [];
@@ -25,8 +56,8 @@ function [new_start, data_time, data_seg] = patterMatch(ev_value, rhythm_number)
 
     j=1;
     % 计算xcorr值
-    for i= 1:100:length(ev_value)-2500
-        heng(j,:) = i:i+2500;
+    for i= 1:100:length(ev_value)-1500
+        heng(j,:) = i:i+1500;
         zong = ev_value(heng(j,:));
         c(j,:) = xcorr(L,zong,'normalized');
         j=j+1;
@@ -34,7 +65,7 @@ function [new_start, data_time, data_seg] = patterMatch(ev_value, rhythm_number)
 
     M1 = max(c,[],2);
     [M2,M2i] = sort(M1);
-    Mx = M2i(length(M2)-rhythm_number*6:length(M2));
+    Mx = M2i(length(M2)-rhythm_number*5:length(M2));
 
     % 取出比对那一段的开始点
     for ii = 1:length(Mx)
@@ -61,9 +92,9 @@ function [new_start, data_time, data_seg] = patterMatch(ev_value, rhythm_number)
     % 根据起始点，找距离它前后的最大值点，作为真正segmentation的起始点，然后取值
     for iii = 1:length(start_point_avg)
         if start_point_avg(iii)-450 > 0
-            heng2(iii,:) = start_point_avg(iii)-450:start_point_avg(iii)+50;
+            heng2(iii,:) = start_point_avg(iii)-250:start_point_avg(iii)+50;
         else
-            heng2(iii,:) = [ones(1,501 - (start_point_avg(iii)+50)),1:start_point_avg(iii)+50];
+            heng2(iii,:) = [ones(1,301 - (start_point_avg(iii)+50)),1:start_point_avg(iii)+50];
         end
         zong2(iii,:) = ev_value(heng2(iii,:));
     end
@@ -91,6 +122,6 @@ function [new_start, data_time, data_seg] = patterMatch(ev_value, rhythm_number)
     end
 
     for jj = 1:length(new_start)
-        data_seg(jj,:) = ev_value(new_start(jj):new_start(jj)+2500);
-        data_time(jj,:) = time(new_start(jj):new_start(jj)+2500);
+        data_seg(jj,:) = ev_value(new_start(jj):new_start(jj)+1500);
+        data_time(jj,:) = time(new_start(jj):new_start(jj)+1500);
     end
